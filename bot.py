36249872d12d02e2c11915c2eacce24d0db955da
horizon-loader <iworeal5@gmail.com>
2026-07-19 02:53:40 +0300

    bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+import os
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+bot = Bot(token=os.getenv('TOKEN'))
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.reply("hello, soon yellowpeople.cc in this bot")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
